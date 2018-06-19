8cae3896dd00efe38bbf774d2c94284a014bcb5d
yxc <1459177541@qq.com>
2018-06-19 19:08:30 +0800

    update dir

---
+++ test/test/TestImp.java
@@ -0,0 +1,20 @@
+package test;
+
+import frame.proxy.ProxyImp;
+import frame.proxy.annotation.AfterReturn;
+import frame.proxy.annotation.Before;
+
+public interface TestImp extends ProxyImp {
+	@Before(methodClassName = { "aopTest" })
+	@AfterReturn(methodClassName = { "aopTest" })
+	public void print();
+
+	@Before(methodClassName = { "aopTest" })
+	@AfterReturn(methodClassName = { "aopTest" })
+	public void print(int arg, int arg2);
+
+	@Before(methodClassName = { "aopTest" })
+	@AfterReturn(methodClassName = { "aopTest" })
+	public int add(int arg, int arg2);
+
+}
